b1e7507aba7efa8f0faa5f019b3941a0fea682b5
divy-03 <127951881+divy-03@users.noreply.github.com>
2025-06-03 19:15:37 +0530

    added upvote and downvote routes

---
+++ src/app/api/streams/upvote/route.ts
@@ -0,0 +1,34 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+const UpvoteSchema = z.object({
+  streamId: z.string(),
+});
+
+export async function POST(req: Request) {
+  const session = await getServerSession();
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? "",
+    },
+  });
+
+  if (!user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const data = await UpvoteSchema.parseAsync(await req.json());
+    await prismaClient.upvote.create({
+      data: {
+        userId: user.id,
+        streamId: data.streamId,
+      },
+    });
+  } catch (error) {
+    console.error("Error upvoting stream:", error);
+    return new Response("Error while upvoting a stream", { status: 500 });
+  }
+}
